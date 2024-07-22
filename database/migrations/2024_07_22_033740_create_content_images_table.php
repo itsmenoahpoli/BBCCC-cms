56ff36040f867fb893156a3b122b1984e0ff1314
Patrick Noah Policarpio <patrickpolicarpio08@gmail.com>
2024-07-22 12:30:26 +0800

    added initial models for content image/video

---
+++ database/migrations/2024_07_22_033740_create_content_images_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('content_images', function (Blueprint $table) {
+            $table->id();
+            $table->string('content_uid')->unique();
+            $table->string('name')->unique();
+            $table->string('name_slug')->unique();
+            $table->string('file_dir');
+            $table->enum('status', ['draft', 'published', 'archived'])->default('draft');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('content_images');
+    }
+};
